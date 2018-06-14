1efbb9477af80dde5c574d446e9273bafdfdde19
Daniel Gao <danielgaooooo@gmail.com>
2018-06-14 16:11:16 -0400

    added exercise service

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Exercise search</h1>
+                <input/>
+                <button>Search</button>
+            </div>
+        )
+    }
+}
+
+
